d56701a6cfa0f1062ede3f09f530f7b1ce815f6f
Isaac Snow <isaacjsnow@gmail.com>
2025-12-03 16:30:02 -0800

    ok

--- src/routes/api/auth/$.ts
+++ src/routes/api/auth/$.ts
@@ -1,15 +1,30 @@
-import { createFileRoute } from "@tanstack/react-router";
+// import { createFileRoute } from "@tanstack/react-router";
 
 import { auth } from "@/auth";
+
+// export const Route = createFileRoute("/api/auth/$")({
+// 	server: {
+// 		handlers: {
+// 			GET: async ({ request }: { request: Request }) => {
+// 				return await auth.handler(request);
+// 			},
+// 			POST: async ({ request }: { request: Request }) => {
+// 				return await auth.handler(request);
+// 			},
+// 		},
+// 	},
+// });
+
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/api/auth/$")({
 	server: {
 		handlers: {
-			GET: async ({ request }: { request: Request }) => {
-				return await auth.handler(request);
+			GET: ({ request }) => {
+				return auth.handler(request);
 			},
-			POST: async ({ request }: { request: Request }) => {
-				return await auth.handler(request);
+			POST: ({ request }) => {
+				return auth.handler(request);
 			},
 		},
 	},
