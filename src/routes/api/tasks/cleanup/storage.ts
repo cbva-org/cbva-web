0b87d9485518149836a3cfffa17446e9987a4114
Isaac Snow <isaacjsnow@gmail.com>
2025-12-16 18:02:37 -0800

    hello

--- src/routes/api/tasks/cleanup/storage.ts
+++ src/routes/api/tasks/cleanup/storage.ts
@@ -1,4 +1,51 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { internalServerError } from "@/lib/responses";
+import { getSupabaseServerClient } from "@/supabase/server";
+import { isNotNullOrUndefined } from "@/utils/types";
+
+async function walk(
+	supabase: ReturnType<typeof getSupabaseServerClient>,
+	bucketName: string,
+	path?: string,
+) {
+	const files = await supabase.storage.from(bucketName).list(path);
+	if (files.error) {
+		throw internalServerError(files.error.message);
+	}
+
+	const paths: { path: string; createdAt: string; updatedAt: string }[] = [];
+
+	for (const file of files.data) {
+		const currPath = [path, file.name].filter(isNotNullOrUndefined).join("/");
+		if (file.id === null) {
+			const sub = await walk(supabase, bucketName, currPath);
+
+			paths.push(...sub);
+		} else {
+			paths.push({
+				path: currPath,
+				createdAt: file.created_at,
+				updatedAt: file.updated_at,
+			});
+		}
+	}
+
+	return paths;
+}
+
+const bucketLookupPaths: { [key: string]: string[] } = {
+	venues: ["headerImageSource", "thumbnailImageSource"],
+};
+
+async function cleanupBucket(
+	supabase: ReturnType<typeof getSupabaseServerClient>,
+	bucketName: string,
+	objects: { path: string; createdAt: string; updatedAt: string }[],
+) {
+	const lookupPaths = bucketLookupPaths[bucketName];
+
+	// ...
+}
 
 export const Route = createFileRoute("/api/tasks/cleanup/storage")({
 	server: {
@@ -9,6 +56,19 @@ export const Route = createFileRoute("/api/tasks/cleanup/storage")({
 				// - check each location a file is stored to see if it is referenced
 				// - if not, delete the objects
 
+				const supabase = getSupabaseServerClient();
+
+				const result = await supabase.storage.listBuckets();
+				if (result.error) {
+					throw internalServerError(result.error.message);
+				}
+
+				for (const bucket of result.data) {
+					const paths = await walk(supabase, bucket.name);
+
+					console.log(bucket.name, paths);
+				}
+
 				return new Response(JSON.stringify({ success: true }), {
 					headers: {
 						"Content-Type": "application/json",
@@ -18,3 +78,5 @@ export const Route = createFileRoute("/api/tasks/cleanup/storage")({
 		},
 	},
 });
+
+// ---------------------------------
